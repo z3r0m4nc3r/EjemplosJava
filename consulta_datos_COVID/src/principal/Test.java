64e006b5ba2e38c95a76c86b08500316ffab0a19
z3r0m4nc3r <domingo.sd78@gmail.com>
2020-07-24 14:00:55 +0200

    24/07/2020 14:00

--- consulta_datos_COVID/src/principal/Test.java
+++ consulta_datos_COVID/src/principal/Test.java
@@ -1,5 +1,8 @@
 package principal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -7,19 +10,42 @@ import service.CovidService;
 import util.Utilidades;
 
 public class Test {
-
+	static CovidService service = new CovidService();
 	public static void main(String[] args) {
 		
 		picoContagios();
+		mediaPositivosDiarios();
+		totalPositivosComunidad();
 
 
 	}
 	
 	static void picoContagios() {
-		CovidService service = new CovidService();
+	
 		LocalDate dia = Utilidades.dateToLocalDate(service.picoContagios());
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		System.out.println("Fecha en la que se notificaron más contagios: "+dia.format(dtf));
+	}
+	
+	static void mediaPositivosDiarios() {
+		System.out.println("La media de positivos diarios hasta el 20/07/2020 es de: "+service.mediaPositivosDiarios());
+	}
+	
+	static void totalPositivosComunidad() {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader bf=new BufferedReader(isr);
+		System.out.println("Introduzca Comunidad Autonoma:");
+		String ciudad;
+		try {
+			ciudad = bf.readLine();
+			System.out.println("El total de casos notificados en "+ciudad+" es de "+service.totalPositivosComunidad(ciudad));
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+			
+			
+		
 	}
 
 }
